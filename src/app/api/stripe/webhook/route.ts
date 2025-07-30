bf7fc7686e99c0693c16cbc427449b15451339e5
Priapisman677 <expeliamm02@hotmail.com>
2025-07-29 17:53:07 -0700

    update

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -1,7 +1,22 @@
+import { stripe } from "@/lib/stripe";
+import { headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-    const bodyBinary = await req.text()
+    const bodyBinary = await req.text() //? Might need extra configuration to receive in binary form
+
+    const headerStore = await headers()
+
+    const binaryHeader = headers().get('stripe-signature')
+
+
+    let event
+
+    try{
+        event = stripe.webhooks.constructEvent(bodyBinary, )
+    }catch(e){
+        console.log(e);	
+    }
 
     console.log({bodyBinary});
     
